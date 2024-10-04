a93a91c96d3aa3d5a604ee817ad5b606296ccb3d
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-10-04 01:40:32 -0500

    Fix colors file bug

--- src/global.d.ts
+++ src/global.d.ts
@@ -17,3 +17,22 @@ declare module "@/assets/imgs/assetsManager" {
   const assets: { [key: string]: string };
   export default assets;
 }
+
+declare module "@/data/config/colors" {
+  export const colors: {
+    primary: {
+      lighter: string;
+      light: string;
+      main: string;
+      dark: string;
+      darker: string;
+    };
+    secondary: {
+      lighter: string;
+      light: string;
+      main: string;
+      dark: string;
+      darker: string;
+    };
+  };
+}
