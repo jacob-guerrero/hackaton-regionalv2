28c6dd3eada29145d97c87ec6e37696ee0df0c41
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-10-04 01:38:27 -0500

    Change colors file

---
+++ src/data/config/colors.ts
@@ -0,0 +1,32 @@
+/* Configure colors at https://shipixen.com/color-theme-explorer-shadcn */
+export const colors: {
+  primary: {
+    lighter: string;
+    light: string;
+    main: string;
+    dark: string;
+    darker: string;
+  };
+  secondary: {
+    lighter: string;
+    light: string;
+    main: string;
+    dark: string;
+    darker: string;
+  };
+} = {
+  primary: {
+    lighter: "#f9a8d4",
+    light: "#f472b6",
+    main: "#ec4899",
+    dark: "#db2777",
+    darker: "#be185d",
+  },
+  secondary: {
+    lighter: "#a5b4fc",
+    light: "#818cf8",
+    main: "#6366f1",
+    dark: "#4f46e5",
+    darker: "#4338ca",
+  },
+};
