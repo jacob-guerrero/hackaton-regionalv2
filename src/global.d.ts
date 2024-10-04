70488359382fe08ef5f10295dd7078f3c757658f
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-10-04 01:04:50 -0500

    Declare assetsManager file

--- src/global.d.ts
+++ src/global.d.ts
@@ -12,3 +12,8 @@ declare module "*.png" {
   const value: string;
   export default value;
 }
+
+declare module "@/assets/imgs/assetsManager" {
+  const assets: { [key: string]: string };
+  export default assets;
+}
